8688e721a7cc9b7f705ee34749dbfb7099c8aa45
hyosung <amok87@nate.com>
2022-05-25 15:21:28 +0900

    .

---
+++ front-ui/src/main/java/com/inbiznetcorp/visible/ars/front/ui/framework/resttemplate/RestTemplateClient.java
@@ -0,0 +1,27 @@
+package com.inbiznetcorp.visible.ars.front.ui.framework.resttemplate;
+
+
+import org.json.simple.JSONObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+public class RestTemplateClient
+{
+
+	public static String sender(String url, JSONObject body)
+	{
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		 HttpEntity<String> entity = new HttpEntity<>(body.toString(), headers);
+
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response = restTemplate.postForEntity(url, entity, String.class);
+
+		return response.getBody();
+	}
+
+
+}
