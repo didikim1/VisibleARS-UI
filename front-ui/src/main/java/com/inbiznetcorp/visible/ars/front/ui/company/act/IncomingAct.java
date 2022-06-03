ad63ccb8710323ea1198fb6b5c5148b31883054a
hyosung <amok87@nate.com>
2022-06-03 10:19:27 +0900

    .

--- front-ui/src/main/java/com/inbiznetcorp/visible/ars/front/ui/company/act/IncomingAct.java
+++ front-ui/src/main/java/com/inbiznetcorp/visible/ars/front/ui/company/act/IncomingAct.java
@@ -1,5 +1,6 @@
 package com.inbiznetcorp.visible.ars.front.ui.company.act;
 
+import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.inbiznetcorp.visible.ars.front.ui.company.inbiznet.biz.InbiznetTTsMessage;
 import com.inbiznetcorp.visible.ars.front.ui.framework.beans.FrameworkBeans;
 import com.inbiznetcorp.visible.ars.front.ui.framework.mymap.MyMap;
+import com.inbiznetcorp.visible.ars.front.ui.framework.resttemplate.RestTemplateClient;
+import com.inbiznetcorp.visible.ars.front.ui.framework.utils.FrameworkUtils;
 
 @Controller
 @RequestMapping("/incoming")
@@ -28,6 +31,29 @@ public class IncomingAct
 		System.out.println("phoneNumber : " + phoneNumber);
 		System.out.println("phoneNumber : " + phoneNumber);
 		System.out.println("phoneNumber : " + phoneNumber);
+
+		JSONObject body 			= new JSONObject();
+
+
+		// 휴대폰번호 sesison에 저장
+
+		// W 시 [SPRING] Controller에서 redirect해서 메인페이지로 이동
+
+		// F시 감사합니다.로 페이지이동
+
+
+//		System.out.println("-");
+//		System.out.println("API요청 : " + body.toString());
+//		System.out.println("API로부터 전달 받은 응답: "+RestTemplateClient.sender("https://local.ring2pay.com:39030/incoming/phoneNumber", body));
+
+		/*
+		 * body.put("requestNumber", FrameworkUtils.generateSessionID());
+		body.put("requestTime", FrameworkUtils.currentDate());
+		body.put("callInfo", body_callInfo);
+		body_tts.put("intro", InbiznetTTsMessage.mCodeToTTSMessage.get(ttsKey));
+		body.put("tts", body_tts);
+		RestTemplateClient.sender("https://local.ring2pay.com:39030//api/v1/asterisk/event/playBack.do", body);
+		 */
 
 		return pagePrefix + "inbiznet" +"/Main";
 	}
